028f829f17a256d932f181e7ca3acaa8ac99df0c
Documenter.jl <documenter@juliadocs.github.io>
2021-08-16 08:42:28 +0000

    build based on a5e6a07

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BetaDisp","category":"page"},{"location":"#BetaDisp","page":"Home","title":"BetaDisp","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BetaDisp.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BetaDisp]","category":"page"}]
+}
