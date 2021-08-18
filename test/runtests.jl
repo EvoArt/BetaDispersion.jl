f94e006503ee3238ffc4bddf5f48c6f028b22d9d
Arthur <arn203@exeter.ac.uk>
2021-08-18 16:56:05 +0100

    time trials

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,11 @@
-using BetaDisp
-using Test, RCall
+using BetaDisp, Distances, StatsFuns
+using Test
 
 @testset "BetaDisp.jl" begin
-    # Write your tests here.
+    x = rand(1000,50)
+    d = dispersion(x,rand(1:5,1000))
+    bench = @benchmark dispersion(x,rand(1:5,1000), BrayCurtis)
+    @test mean(bench.times) .< 0.1
+    bench = @benchmark permutest(d)
+    @test mean(bench.times) .< 1
 end
