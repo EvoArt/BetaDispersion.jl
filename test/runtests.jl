be4f42a55a3c7248fb3dca655f94fd3c2eb7793e
Arthur <arn203@exeter.ac.uk>
2021-08-27 23:30:39 +0100

    remove irrelevant dist metric from tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,10 +2,10 @@ using BetaDisp, Distances, StatsBase,BenchmarkTools
 using Test
 
 #@testset "BetaDisp.jl" begin
-    x = rand(30,50) .*y
+    x = rand(30,50) 
     y =rand(1:5,30)
     d = dispersion(x,y,Euclidean)
-    permutest(d)
+    permutest(d, n_perm = 1000)
     
     bench = @benchmark dispersion($x,$y, BrayCurtis)
     @test mean(bench.times) .< 2
