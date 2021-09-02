66c77fbb95cead81baf5f3b8e45e89cacd71c20a
Documenter.jl <documenter@juliadocs.github.io>
2021-09-02 14:08:53 +0000

    build based on 6250d08

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BetaDisp","category":"page"},{"location":"#BetaDisp","page":"Home","title":"BetaDisp","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BetaDisp.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BetaDisp]","category":"page"}]
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BetaDispersion","category":"page"},{"location":"#BetaDispersion","page":"Home","title":"BetaDispersion","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BetaDispersion.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BetaDispersion]","category":"page"}]
 }
