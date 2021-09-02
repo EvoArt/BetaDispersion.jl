a0e183f64d59e4672876129234d4990308a7d810
Arthur <arn203@exeter.ac.uk>
2021-09-02 11:53:45 +0100

    add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,27 +1,14 @@
 using BetaDisp, Distances, StatsBase,BenchmarkTools
 using Test
 
-#@testset "BetaDisp.jl" begin
-    x = rand(30,50) 
-    y =rand(1:3,30)
+@testset "BetaDisp.jl" begin
+    x = rand(500,50) 
+    y =rand(1:10,500)
     d = dispersion(x,y,Euclidean)
-    permutest(d, 10000)
-    dd = dispersion(x,y, metric = true)
+    dispersion(x,y, metric = true)
     bench = @benchmark dispersion($x,$y, BrayCurtis)
-    @test mean(bench.times) .< 2
+    @test mean(bench.times) < 1*10^8
     bench = @benchmark permutest($d)
-    @test mean(bench.times) .< 3
+    @test mean(bench.times) < 1*10^9
     
-   distfuns =  [
-    Euclidean,
-    Cityblock,
-    TotalVariation,
-    Chebyshev,
-    Minkowski,
-    Jaccard,
-    BrayCurtis]
-    for distfun in distfuns
-         @test typeof(dispersion(x,rand(1:5,100), distfun)) <: Disp
-    end
-
-#end
+end
