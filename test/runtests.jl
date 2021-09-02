32200cd94aa866f3024bdafc101ac111f1d8bafe
Arthur <arn203@exeter.ac.uk>
2021-09-02 13:21:38 +0100

    larger test array

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,13 +2,13 @@ using BetaDispersion, Distances, StatsBase,BenchmarkTools
 using Test
 
 @testset "BetaDisp.jl" begin
-    x = rand(500,50) 
-    y =rand(1:10,500)
+    x = rand(1000,50) 
+    y =rand(1:10,1000)
     d = dispersion(x,y,Euclidean)
     dispersion(x,y, metric = true)
     bench = @benchmark dispersion($x,$y, BrayCurtis)
-    @test mean(bench.times) < 1*10^8
-    bench = @benchmark permutest($d)
     @test mean(bench.times) < 1*10^9
+    bench = @benchmark permutest($d)
+    @test mean(bench.times) < 2*10^9
     
 end
