5ed0270329647929bb2accd9d6cb68d2240ca292
Arthur <arn203@exeter.ac.uk>
2021-10-18 13:35:40 +0100

    beta likelihood

--- src/Bayes.jl
+++ src/Bayes.jl
@@ -1,5 +1,11 @@
 
 using Turing
+
+function param(α,β)
+    μ = α/(α+β)
+    σ² = (α*β)/((α+β)^2 *(α+β+1))
+    return [μ σ²]
+end
     @model function BayesDisp(R,μ_scale = 1)
         k = length(R)      
         μ ~ filldist(truncated(Normal(0,μ_scale),0,Inf),k)
@@ -9,8 +15,15 @@ using Turing
         end
     end
 
-
-function bayesdisp(disp,μ_scale = 1.0,n_samples = 1000,n_chains = 4, sampler = NUTS())
+    @model function BetaDisp(R)
+        k = length(R) 
+        α ~ filldist(Uniform(1,100),k)#Gamma(2,2)
+        β ~ filldist(Uniform(1,100),k)#Gamma(2,2)
+        for i in 1:k
+            R[i] .~ Beta(α[i],β[i])
+        end     
+    end
+function bayesdisp(disp,μ_scale,n_samples = 1000,n_chains = 4, sampler = NUTS())
     mod = BayesDisp(disp.residuals, μ_scale)
     chns = sample(mod,sampler, MCMCThreads(),n_samples,n_chains)
     D = Dict()
@@ -22,6 +35,18 @@ function bayesdisp(disp,μ_scale = 1.0,n_samples = 1000,n_chains = 4, sampler = 
     return replacenames(chns, D)
 end
 
-export bayesdisp
+function bayesdisp(disp,n_samples = 1000,n_chains = 4, sampler = NUTS())
+    mod = BetaDisp(disp.residuals)
+    chns = sample(mod,sampler, MCMCThreads(),n_samples,n_chains)
+    D = Dict()
+    for (i,level) in enumerate(string.(disp.levels))
+        D["α[$(i)]"] = level * "_μ"
+        D["β[$(i)]"] = level * "_σ"
+    end
+    
+    return replacenames(chns, D)
+end
+
+export bayesdisp, param
 
     
