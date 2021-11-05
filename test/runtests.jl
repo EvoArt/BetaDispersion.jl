dc908b61d789858576d2d3e170171227c9cbde0a
Arthur <arn203@exeter.ac.uk>
2021-11-05 10:09:45 +0000

    dist matrix in test

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,11 +4,12 @@ R"library(vegan)"
 
    x = rand(1000,100) 
    y =rand(1:20,1000)
+   j = @benchmark dispersion($x,$y,BrayCurtis) 
+   r = @benchmark R"betadisper(vegdist($x),as.factor($y))" 
+   @test mean(j.times) < mean(r.times)
+   
    Dj = pairwise(BrayCurtis(),x,dims = 1) 
    Dr =R"vegdist($x)"   
-   j = @benchmark dispersion(Dj,y) 
-   r = @benchmark R"betadisper($Dr,as.factor($y))" 
-   @test mean(j.times) < mean(r.times)
    dispj = dispersion(Dj,y)
    dispr = R"betadisper($Dr,as.factor($y))"
    j = @benchmark permutest(dispj,9999) 
